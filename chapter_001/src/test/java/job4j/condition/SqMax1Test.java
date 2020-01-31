0565f3645dbff5f9d87369e825f570de1fe21570
AlexandrovAlexandr <9857780008@mail.ru>
2020-01-31 16:52:40 +0200

    Отладка программы в IDEA.[#166136]

--- chapter_001/src/test/java/job4j/condition/SqMax1Test.java
+++ chapter_001/src/test/java/job4j/condition/SqMax1Test.java
@@ -1,39 +1,39 @@
 package job4j.condition;
 
 import org.junit.Test;
-import ru.job4j.condition.SqMax;
+import ru.job4j.condition.SqMax1;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class SqMaxTest {
+public class SqMax1Test {
     @Test
     public void whenSecondMax(){
-        SqMax check = new SqMax();
+        SqMax1 check = new SqMax1();
         int result = check.max(10, 11, 7, 5);
         assertThat(result, is(11));
     }
     @Test
     public void whenFirstMax(){
-        SqMax check = new SqMax();
+        SqMax1 check = new SqMax1();
         int result = check.max(5, 3, 2,0);
         assertThat(result, is(5));
     }
     @Test
     public void whenThirdMax(){
-        SqMax check = new SqMax();
+        SqMax1 check = new SqMax1();
         int result = check.max(6, 4, 7,5);
         assertThat(result, is(7));
     }
     @Test
     public void whenForthMax(){
-        SqMax check = new SqMax();
-        int result = check.max(10, 4, 150,20);
-        assertThat(result, is(150));
+        SqMax1 check = new SqMax1();
+        int result = check.max(2, 8, 4,12);
+        assertThat(result, is(12));
     }
     @Test
     public void whenAllTheSame(){
-        SqMax check = new SqMax();
+        SqMax1 check = new SqMax1();
         int result = check.max(3, 3, 3,3);
         assertThat(result, is(3));
     }
